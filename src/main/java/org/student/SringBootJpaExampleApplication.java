81da96b6d8ce6e6ec8735ab20a342dea5bdae6ea
Madhusudhakar <madhusudhakar@ALIPL2508.local>
2025-01-27 16:44:11 +0530

    sprign boot jpa

---
+++ src/main/java/org/student/SringBootJpaExampleApplication.java
@@ -0,0 +1,13 @@
+package org.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SringBootJpaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SringBootJpaExampleApplication.class, args);
+	}
+
+}
---
+++ src/main/java/org/student/entity/User.java
@@ -0,0 +1,5 @@
+package org.student.entity;
+
+
+public class User {
+}
